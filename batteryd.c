30366ed476a6060ee022260b6e9024d46b085a39
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-08-23 17:17:36 +0300

    Allow access to socket for the batteryd group and improve error reporting.

--- batteryd.c
+++ batteryd.c
@@ -10,6 +10,14 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include <inttypes.h>
 #include <grp.h>
 
+enum service_status
+{
+    SUCCESS,
+    VALUE_TOO_SMALL,
+    VALUE_TOO_LARGE,
+    SYSTEM_FAILURE
+};
+
 int set_battery_charge_threshold(int8_t threshold)
 {
     char *ctrl = "/sys/class/power_supply/BAT0/charge_control_end_threshold";
@@ -18,7 +26,12 @@ int set_battery_charge_threshold(int8_t threshold)
     else if (threshold > 100)
         return 2;
     FILE *control = fopen(ctrl, "w");
-    fprintf(control, "%" PRId8, threshold);
+    int chars_written = fprintf(control, "%" PRId8, threshold);
+    if (chars_written <= 0)
+    {
+        perror("Failed to set threshold");
+        return 3;
+    }
     fclose(control);
     return 0;
 }
@@ -27,18 +40,27 @@ int main(void)
 {
     struct sockaddr_un srv_socket = {.sun_family = AF_UNIX, .sun_path = "/run/batteryd"};
     int srv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    // Get the GID of the batteryd group to allow access of group members to socket
     struct group *grp;
-    grp = getgrnam("bctrl");
+    grp = getgrnam("batteryd");
+    if (grp == NULL)
+    {
+        fputs("Failed to get group batteryd\n", stderr);
+        return 2;
+    }
 
     if (bind(srv_fd, (struct sockaddr *)(&srv_socket), sizeof(srv_socket)) == -1)
     {
-        fputs("Failed to bind the unix socket...", stderr);
+        perror("Failed to bind the unix socket");
         return 1;
     }
+    // Set the socket to be rw for root and the batteryd group
     fchmod(srv_fd, 660);
+    fchown(srv_fd, 0, grp->gr_gid);
     if (listen(srv_fd, 1) == -1)
     {
-        fputs("Failed to start listener...", stderr);
+        perror("Failed to start listener");
         return 1;
     }
     while (1)
