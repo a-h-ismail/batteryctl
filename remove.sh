1664be3e1cf2a7a4715bd580e51540365a609865
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-09-06 16:36:35 +0300

    Add uninstall script.

---
+++ remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+else
+  echo 'Removing battery charge threshold service.'
+fi
+systemctl disable --now batteryd.service
+rm /usr/local/bin battery{d,c} /etc/batteryd.conf /usr/lib/systemd/system/batteryd.service
+systemctl daemon-reload
+echo 'Done!'
